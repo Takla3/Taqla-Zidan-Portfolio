2034f0ebfdcadcf418e894d7b6bf8a28604f23ea
Takla Zidan <ztakla3@gmail.com>
2025-12-14 23:27:22 +0300

    fix home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,27 +7,24 @@ import ThemeToggle from './ThemeToggle';
 function Home({ setActiveNav }) {
     return (
         <section className="home" id="home">
-            <div className='home'>
-                <ThemeToggle />
-                <div className='container home-container'>
-                    <h4>Hello I'm </h4>
-                    <h1 className="gradient-name">Taqla Zidan</h1>
-                    <h4 className='text-light'>Backend Developer</h4>
-                    <div className='btns'>
-                        <a href={CV} className='btn' download>Download CV</a>
-                        <a href='#contact' className='btn btn-primary' onClick={() => setActiveNav('#contact')}>Let's Talk</a>
-
-                    </div>
-                    <div className='me'>
-                        <img src={Me} alt="" />
-                    </div>
-                    <a href='#about' className='scroll_down' onClick={() => setActiveNav('#about')}>Scroll Down</a>
-                    <HomeSocials />
+            <ThemeToggle />
+            <div className='container home-container'>
+                <h4>Hello I'm </h4>
+                <h1 className="gradient-name">Taqla Zidan</h1>
+                <h4 className='text-light'>Backend Developer</h4>
+                <div className='btns'>
+                    <a href={CV} className='btn' download>Download CV</a>
+                    <a href='#contact' className='btn btn-primary' onClick={() => setActiveNav('#contact')}>Let's Talk</a>
 
                 </div>
-
+                <div className='me'>
+                    <img src={Me} alt="" />
+                </div>
+                <a href='#about' className='scroll_down' onClick={() => setActiveNav('#about')}>Scroll Down</a>
+                <HomeSocials />
 
             </div>
+
         </section>
     )
 }
